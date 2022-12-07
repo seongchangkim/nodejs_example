a78993591cd85ab20b4aee58712d624f1fcd9c22
DunKim <darkgx21@gmail.com>
2022-12-08 00:35:37 +0900

    개인 정보 보호

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const config = require('./config/key')
+
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://dunkim:1234@example.bsoyb4s.mongodb.net/?retryWrites=true&w=majority', {
+const { mongoURI } = require('./config/dev')
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   // useCreateIndex: true,
@@ -14,7 +26,21 @@ mongoose.connect('mongodb+srv://dunkim:1234@example.bsoyb4s.mongodb.net/?retryWr
   .catch((err) => console.error(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!~~안녕하세요~')
+  res.send('Hello World!~')
+})
+
+app.post('/register', (req, res) => {
+  // 회원 가입할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success : false, err})
+
+    return res.status(200).json({
+      success : true
+    })
+  })
+
 })
 
 app.listen(port, () => {
